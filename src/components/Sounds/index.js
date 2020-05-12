b21f959efbce5152fddc256b69293722a4a0ae01
Xavier Pladevall <xavier.pladevall@columbia.edu>
2020-05-11 18:55:30 -0700

    added new sounds

--- src/components/Sounds/index.js
+++ src/components/Sounds/index.js
@@ -15,34 +15,79 @@ import rooster from "./rooster.mp3";
 import train from "./train.mp3";
 import woosh from "./woosh.mp3";
 import trex from "./trex.mp3";
+/////////////////////
+import igotabadfeeling from "./IGotABadFeelingAboutThis.mp3";
+import ohmygod from "./OhMyGod.mp3";
+import evillaugh from "./EvilLaugh.mp3";
+import boring from "./Boring.mp3";
+import ohhellno from "./Ohhellno.mp3";
+import triggered from "./Triggered.mp3";
+import tada from "./Tada.mp3";
+import slap from "./Slap.mp3";
+import missionimpossible from "./MissionImpossible.mp3";
+import hideyokids from "./HideYoKids.mp3";
+import wasted from "./Wasted.mp3";
+import itwasatthismoment from "./ItWasAtThisMomentHeKnew.mp3";
+import iamtheone from "./IAmTheOne.mp3";
+import aintnobodygottimeforthat from "./AintNobodyGotTimeForThat.mp3";
+import catchmeoutside from "./CatchMeOutside.mp3";
+import gibberish from "./Gibberish.mp3";
+import surprisemotherfucker from "./SurpriseMotherfucker.mp3";
 
-// -Gong
-// - Incoming suspense (soundbible)
-// - oh hell no
-// - a few moments later
-// - monkey, dog, cat, animals
-
-// 😂 Face with Tears of Joy
-// 😭 Loudly Crying Face
-// 🥺 Pleading Face
-// 🤣 Rolling on the Floor Laughing
-// ❤️ Red Heart
-// ✨ Sparkles
-// 😍 Smiling Face with Heart-Eyes
-// 🙏 Folded Hands
-// 😊 Smiling Face with Smiling Eyes
-// 🥰 Smiling Face with Hearts
-// 👍 Thumbs Up
-// 💕 Two Hearts
-// 🤔 Thinking Face
-
-// INTERESTING EMOJIS
-// 🥱 Yawning Face
-// 🤯 Mindblown
-// 👌 OK
-// 🙌 Wohoo
-// 🤠 yeeha
-// 🤑 money, mopney money
+import toinfinity from "./ToInfinity&Beyond.mp3";
+import squeak from "./Squeak.mp3";
+import hahahawtf from "./HahahaWTF.mp3";
+import waitaminute from "./WaitaMinute.mp3";
+import saywhat from "./SayWhat.mp3";
+import dreamharp from "./DreamHarp.mp3";
+import dialup from "./DialUp.mp3";
+import recordscratch from "./RecordScratch.mp3";
+import meepmeep from "./MeepMeep.mp3";
+import golfswing from "./GolfSwing.mp3";
+import laugh from "./Laugh.mp3";
+import ohh from "./Ohh.mp3";
+import nope from "./Nope.mp3";
+import nonono from "./NoNoNo.mp3";
+import dundundun from "./DunDunDun.mp3";
+import johncena from "./JohnCena.mp3";
+import nice from "./Nice.mp3";
+import drumroll from "./Drumroll.mp3";
+import heavenlymusic from "./HeavenlyMusic.mp3";
+import circus from "./Circus.mp3";
+import deeznuts from "./DeezNuts.mp3";
+import crickets from "./Crickets.mp3";
+import fbi from "./FBIOpenUp.mp3";
+import shotsfired from "./ShotsFired.mp3";
+import justdoit from "./JustDoIt.mp3";
+import sad from "./Sad.mp3";
+import aww from "./Aww.mp3";
+import ok from "./Ok.mp3";
+import cheering from "./Cheering.mp3";
+import illuminati from "./Illuminati.mp3";
+import runrun from "./RunRun.mp3";
+import wiitheme from "./WiiTheme.mp3";
+import elevatormusic from "./ElevatorMusic.mp3";
+import afewmomentslater from "./AFewMomentsLater.mp3";
+import nopleaseno from "./NoPleaseNo.mp3";
+import bruh from "./Bruh.mp3";
+import garbage from "./StillAPieceofGarbage.mp3";
+import cork from "./Cork.mp3";
+import buzzer from "./Buzzer.mp3";
+import oopsie from "./Oopsie.mp3";
+import toasty from "./Toasty.mp3";
+import thuglife from "./ThugLife.mp3";
+import lovemoment from "./LoveMoment.mp3";
+import dolphin from "./Dolphin.mp3";
+import windowserror from "./WindowsError.mp3";
+import lion from "./Lion.mp3";
+import waitinggame from "./Nowweplaythewaitinggame.mp3";
+import yeehaw from "./Yeehaw.mp3";
+import gameover from "./GameOver.mp3";
+import sheep from "./Sheep.mp3";
+import gtfo from "./GTFO.mp3";
+import terminator from "./Terminator.mp3";
+import noidea from "./NoIdea.mp3";
+import knock from "./Knock.mp3";
 
 export {
   snare,
@@ -62,4 +107,122 @@ export {
   train,
   woosh,
   trex,
+  //
+  igotabadfeeling,
+  ohmygod,
+  evillaugh,
+  boring,
+  ohhellno,
+  triggered,
+  tada,
+  slap,
+  missionimpossible,
+  hideyokids,
+  wasted,
+  itwasatthismoment,
+  iamtheone,
+  aintnobodygottimeforthat,
+  catchmeoutside,
+  gibberish,
+  surprisemotherfucker,
+  toinfinity,
+  squeak,
+  hahahawtf,
+  waitaminute,
+  saywhat,
+  dreamharp,
+  dialup,
+  recordscratch,
+  meepmeep,
+  golfswing,
+  laugh,
+  ohh,
+  nope,
+  nonono,
+  dundundun,
+  johncena,
+  nice,
+  drumroll,
+  heavenlymusic,
+  circus,
+  deeznuts,
+  crickets,
+  fbi,
+  shotsfired,
+  justdoit,
+  sad,
+  aww,
+  ok,
+  cheering,
+  illuminati,
+  runrun,
+  wiitheme,
+  elevatormusic,
+  afewmomentslater,
+  nopleaseno,
+  bruh,
+  garbage,
+  cork,
+  buzzer,
+  oopsie,
+  toasty,
+  thuglife,
+  lovemoment,
+  dolphin,
+  windowserror,
+  lion,
+  waitinggame,
+  yeehaw,
+  gameover,
+  sheep,
+  gtfo,
+  terminator,
+  noidea,
+  knock,
 };
+
+/////////////////////
+// - Gong
+// - Incoming suspense (soundbible)
+// - monkey, dog, cat, animals
+// - Mario coin, fall sound, dies
+// - Sauce
+// - CHINA (trump)
+// - Ladies anf gentleman we got him
+// - Anotha one
+// - Marvin Gaye - Let's Get It On Sound Effect
+// - You Played Yourself
+// - Developers, developers, developers.
+
+/////////////////////
+// - Meanwhile
+// - Two hours later
+// - a few moments later
+// - An eternity later
+// - 2000 Thousand Years Later
+// -
+// -
+// -
+// -
+// -
+// -
+
+// 😂 Face with Tears of Joy
+// 😭 Loudly Crying Face
+// 🥺 Pleading Face
+// 🤣 Rolling on the Floor Laughing
+// ❤️ Red Heart
+// ✨ Sparkles
+// 😍 Smiling Face with Heart-Eyes
+// 🙏 Folded Hands
+// 😊 Smiling Face with Smiling Eyes
+// 🥰 Smiling Face with Hearts
+// 👍 Thumbs Up
+// 💕 Two Hearts
+// 🤔 Thinking Face
+
+// INTERESTING EMOJIS
+// 🤯 Mindblown
+// 👌 OK
+// 🙌 Wohoo
+// 🤑 money, mopney money
